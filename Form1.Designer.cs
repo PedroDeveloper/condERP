c6c984f577ab34144b4bb0489a720d6af717b51a
Pedro <puedroreis@gmail.com>
2021-03-23 06:27:12 -0300

    fist commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿
+namespace porrraaaaaaa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnNovo = new System.Windows.Forms.ToolStripButton();
+            this.btnEditar = new System.Windows.Forms.ToolStripButton();
+            this.btnSave = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.lbMorador = new System.Windows.Forms.ToolStripLabel();
+            this.boxBuscar = new System.Windows.Forms.ToolStripTextBox();
+            this.btnBuscar = new System.Windows.Forms.ToolStripButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.boxCpf = new System.Windows.Forms.TextBox();
+            this.boxAp = new System.Windows.Forms.TextBox();
+            this.boxVisitante = new System.Windows.Forms.TextBox();
+            this.boxBloco = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnExit = new System.Windows.Forms.ToolStripButton();
+            this.bntDelete = new System.Windows.Forms.ToolStripButton();
+            this.btnCancelar = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnNovo,
+            this.btnEditar,
+            this.btnCancelar,
+            this.bntDelete,
+            this.btnSave,
+            this.toolStripSeparator1,
+            this.lbMorador,
+            this.boxBuscar,
+            this.btnBuscar,
+            this.btnExit});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.toolStrip1.Size = new System.Drawing.Size(456, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnNovo.Image = ((System.Drawing.Image)(resources.GetObject("btnNovo.Image")));
+            this.btnNovo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(23, 22);
+            this.btnNovo.Text = "toolStripButton1";
+            this.btnNovo.ToolTipText = "Novo";
+            this.btnNovo.Click += new System.EventHandler(this.btnNovo_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnEditar.Image = ((System.Drawing.Image)(resources.GetObject("btnEditar.Image")));
+            this.btnEditar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(23, 22);
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnSave.Image = ((System.Drawing.Image)(resources.GetObject("btnSave.Image")));
+            this.btnSave.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(23, 22);
+            this.btnSave.Text = "toolStripButton3";
+            this.btnSave.ToolTipText = "Salvar";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // lbMorador
+            // 
+            this.lbMorador.Name = "lbMorador";
+            this.lbMorador.Size = new System.Drawing.Size(91, 22);
+            this.lbMorador.Text = "Buscar Morador";
+            // 
+            // boxBuscar
+            // 
+            this.boxBuscar.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.boxBuscar.Name = "boxBuscar";
+            this.boxBuscar.Size = new System.Drawing.Size(100, 25);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnBuscar.Image = ((System.Drawing.Image)(resources.GetObject("btnBuscar.Image")));
+            this.btnBuscar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(23, 22);
+            this.btnBuscar.ToolTipText = "Buscar";
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CPF Morador";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(21, 138);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Visitante";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "AP";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Bloco";
+            // 
+            // boxCpf
+            // 
+            this.boxCpf.Location = new System.Drawing.Point(142, 87);
+            this.boxCpf.Name = "boxCpf";
+            this.boxCpf.Size = new System.Drawing.Size(100, 20);
+            this.boxCpf.TabIndex = 5;
+            // 
+            // boxAp
+            // 
+            this.boxAp.Location = new System.Drawing.Point(142, 180);
+            this.boxAp.Name = "boxAp";
+            this.boxAp.Size = new System.Drawing.Size(100, 20);
+            this.boxAp.TabIndex = 6;
+            // 
+            // boxVisitante
+            // 
+            this.boxVisitante.Location = new System.Drawing.Point(142, 135);
+            this.boxVisitante.Name = "boxVisitante";
+            this.boxVisitante.Size = new System.Drawing.Size(100, 20);
+            this.boxVisitante.TabIndex = 7;
+            // 
+            // boxBloco
+            // 
+            this.boxBloco.Location = new System.Drawing.Point(142, 223);
+            this.boxBloco.Name = "boxBloco";
+            this.boxBloco.Size = new System.Drawing.Size(100, 20);
+            this.boxBloco.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(279, 108);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnExit
+            // 
+            this.btnExit.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnExit.Image = ((System.Drawing.Image)(resources.GetObject("btnExit.Image")));
+            this.btnExit.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(23, 22);
+            this.btnExit.Text = "toolStripButton1";
+            this.btnExit.ToolTipText = "Sair";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // bntDelete
+            // 
+            this.bntDelete.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bntDelete.Image = ((System.Drawing.Image)(resources.GetObject("bntDelete.Image")));
+            this.bntDelete.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.bntDelete.Name = "bntDelete";
+            this.bntDelete.Size = new System.Drawing.Size(23, 22);
+            this.bntDelete.Text = "toolStripButton1";
+            this.bntDelete.ToolTipText = "Excluir";
+            this.bntDelete.Click += new System.EventHandler(this.bntDelete_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnCancelar.Image = ((System.Drawing.Image)(resources.GetObject("btnCancelar.Image")));
+            this.btnCancelar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(23, 22);
+            this.btnCancelar.Text = "toolStripButton1";
+            this.btnCancelar.ToolTipText = "Cancelar";
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 286);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.boxBloco);
+            this.Controls.Add(this.boxVisitante);
+            this.Controls.Add(this.boxAp);
+            this.Controls.Add(this.boxCpf);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnNovo;
+        private System.Windows.Forms.ToolStripButton btnEditar;
+        private System.Windows.Forms.ToolStripButton bntDelete;
+        private System.Windows.Forms.ToolStripButton btnSave;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel lbMorador;
+        private System.Windows.Forms.ToolStripTextBox boxBuscar;
+        private System.Windows.Forms.ToolStripButton btnBuscar;
+        private System.Windows.Forms.ToolStripButton btnExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox boxCpf;
+        private System.Windows.Forms.TextBox boxAp;
+        private System.Windows.Forms.TextBox boxVisitante;
+        private System.Windows.Forms.TextBox boxBloco;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripButton btnCancelar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace porrraaaaaaa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            btnBuscar.Enabled = false;
+            btnCancelar.Enabled = false;
+            btnNovo.Enabled = true;
+            btnEditar.Enabled = false;
+            btnBuscar.Enabled = true;
+            boxBuscar.Enabled = true;
+            bntDelete.Enabled = false;
+            btnSave.Enabled = false;
+            boxCpf.Enabled = false;
+            boxVisitante.Enabled = false;
+            boxAp.Enabled = false;
+            boxBloco.Enabled = false;
+            boxCpf.Text = "";
+            boxVisitante.Text = "";
+            boxAp.Text = "";
+            boxBloco.Text = "";
+        }
+
+        SqlConnection sqlCon = null;
+        private string strCon = @"Integrated Security=SSPI;Persist Security Info=False;User ID=pedrop1;Initial Catalog=condominio;Data Source=DESKTOP-135FB4B\SQLEXPRESS1";
+        private string strSql = string.Empty;
+
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            strSql = "insert into liberar_entrada (CPF,visitante,ap,bloco) values (@CPF, @visitante,@ap,@bloco)";
+            sqlCon = new SqlConnection(strCon);
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+            comando.Parameters.Add("@CPF", SqlDbType.Int).Value = boxCpf.Text;
+            comando.Parameters.Add("@visitante", SqlDbType.VarChar).Value = boxVisitante.Text;
+            comando.Parameters.Add("@ap", SqlDbType.VarChar).Value = boxAp.Text;
+            comando.Parameters.Add("@bloco", SqlDbType.VarChar).Value = boxBloco.Text;
+
+            try
+            {
+                sqlCon.Open();
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Visitante cadastrador");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+
+            btnBuscar.Enabled = false;
+            btnNovo.Enabled = true;
+            btnEditar.Enabled = false;
+            btnBuscar.Enabled = true;
+            boxBuscar.Enabled = true;
+            bntDelete.Enabled = false;
+            btnSave.Enabled = true;
+            boxCpf.Enabled = false;
+            boxVisitante.Enabled = false;
+            boxAp.Enabled = false;
+            boxBloco.Enabled = false;
+            boxCpf.Text = "";
+            boxVisitante.Text = "";
+            boxAp.Text = "";
+            boxBloco.Text = "";
+
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+
+            strSql = "select *from liberar_entrada  where CPF = @CPF";
+            sqlCon = new SqlConnection(strCon);
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+            comando.Parameters.Add("@CPF", SqlDbType.Int).Value = boxBuscar.Text;
+     
+
+            try
+            {
+                if (boxBuscar.Text == string.Empty)
+                {
+                    throw new Exception("Digite o CPF do morador");
+                }
+                sqlCon.Open();
+
+                SqlDataReader dr = comando.ExecuteReader();
+
+               
+                if (dr.HasRows == false)
+                {
+                    throw new Exception(" CPF nao cadastrado");
+                }
+
+                dr.Read();
+                boxCpf.Text = Convert.ToString(dr["CPF"]);
+                boxVisitante.Text = Convert.ToString(dr["visitante"]);
+                boxAp.Text = Convert.ToString(dr["ap"]);
+                boxBloco.Text = Convert.ToString(dr["bloco"]);
+
+                MessageBox.Show("liberado");
+
+            }
+            catch (Exception ex)
+      
+            {          MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+
+            strSql = "update liberar_entrada set  CPF = @cpf,visitante = @visitante,ap= @ap,bloco = @bloco where CPF = @idBuscar";
+            sqlCon = new SqlConnection(strCon);
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+            comando.Parameters.Add("@idBuscar", SqlDbType.Int).Value = boxBuscar.Text;
+
+            comando.Parameters.Add("@CPF", SqlDbType.Int).Value = boxCpf.Text;
+            comando.Parameters.Add("@visitante", SqlDbType.VarChar).Value = boxVisitante.Text;
+            comando.Parameters.Add("@ap", SqlDbType.VarChar).Value = boxAp.Text;
+            comando.Parameters.Add("@bloco", SqlDbType.VarChar).Value = boxBloco.Text;
+
+
+            try
+            {
+              
+                sqlCon.Open();                            
+                comando.ExecuteNonQuery();
+                MessageBox.Show("moficado");
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+
+        }
+
+        private void bntDelete_Click(object sender, EventArgs e)
+        {
+
+            strSql = "delete from liberar_entrada where  CPF = @idBuscar";
+            sqlCon = new SqlConnection(strCon);
+            SqlCommand comando = new SqlCommand(strSql, sqlCon);
+
+            comando.Parameters.Add("@idBuscar", SqlDbType.Int).Value = boxBuscar.Text;
+
+    
+
+            try
+            {
+
+                sqlCon.Open();
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Deletado");
+
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlCon.Close();
+            }
+
+        }
+
+        private void btnNovo_Click(object sender, EventArgs e)
+        {
+            btnBuscar.Enabled = false;
+            btnEditar.Enabled = false;
+            btnBuscar.Enabled = false;
+            boxBuscar.Enabled = false;
+            bntDelete.Enabled = false;
+            btnSave.Enabled = true;
+            boxCpf.Enabled = true;
+            boxVisitante.Enabled = true;
+            boxAp.Enabled = true;            
+            boxBloco.Enabled = true;
+            boxCpf.Text = "";
+            boxVisitante.Text = "";
+            boxAp.Text = "";
+            boxBloco.Text = "";
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            btnBuscar.Enabled = false;
+            btnNovo.Enabled = true;
+            btnEditar.Enabled = false;
+            btnBuscar.Enabled = true;
+            boxBuscar.Enabled = true;
+            bntDelete.Enabled = false;
+            btnSave.Enabled = true;
+            boxCpf.Enabled = false;
+            boxVisitante.Enabled = false;
+            boxAp.Enabled = false;
+            boxBloco.Enabled = false;
+            boxCpf.Text = "";
+            boxVisitante.Text = "";
+            boxAp.Text = "";
+            boxBloco.Text = "";
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
